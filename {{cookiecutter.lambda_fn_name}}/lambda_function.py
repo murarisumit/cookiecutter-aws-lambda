52f3464bd6b0020e08db7872f7a68000f1dd32b6
Sumit Murari <sumit.murari@healthify.co>
2018-04-27 10:36:22 +0530

    Removed complicated part, simple to start now

--- {{cookiecutter.lambda_fn_name}}/lambda_function.py
+++ {{cookiecutter.lambda_fn_name}}/lambda_function.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python
-'''
-Parse an SNS event message and send to a Slack Channel
-'''
-import boto3
-from base64 import b64decode
 import json
 import os
-import requests
 
 
 def lambda_handler(event, context):
@@ -18,14 +12,6 @@ def lambda_handler(event, context):
         with open('config.json') as f:
             config = json.load(f)
 
-    # Keep encryped variables in kms.
-    WEBHOOK_URL = "https://" + boto3.client('kms').decrypt(
-        CiphertextBlob=b64decode(config['encrypted_token']))['Plaintext'].decode('utf-8')
-
     # Get normal key
     value = config['key']
-
-    payload = {'text': value}
-    print('DEBUG:', payload)
-    r = requests.post(WEBHOOK_URL, json=payload)
-    return r.status_code
+    return value
--- {{cookiecutter.lambda_fn_name}}/lambda_test.py
+++ {{cookiecutter.lambda_fn_name}}/lambda_test.py
@@ -1,15 +1,16 @@
 import json
 from lambda_function import lambda_handler
-
-json_input = json.loads(r"""
+data = """
 {
   "Records": [
     {
       "EventVersion": "1.0",
       "EventSubscriptionArn": "arn:aws:sns:EXAMPLE",
-      "EventSource": "aws:sns",
-      }
+      "EventSource": "aws:sns"
+    }
   ]
-}""")
-print('running locally')
+
+}"""
+json_input = json.loads(data)
+print("running locally")
 print(lambda_handler(json_input, None))
